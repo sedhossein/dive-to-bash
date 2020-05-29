94a4093a03fcd485a6abef9ea0cebe9778479c94
sedhossein <seyedhossein.hosseini@snapp.cab>
2020-05-29 15:59:27 +0430

    add loops samples

---
+++ loops/1.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+counter=1
+while [ $counter -le 5 ]
+do
+echo $counter
+((counter++))
+done
+# =================================
+counter=1
+until [ $counter -gt 10 ]
+do
+echo $counter
+((counter++))
+done
+# =================================
+names='Stan Kyle Cartman'
+for name in $names
+do
+echo $name
+done
+# =================================
+for value in {1..5}
+do
+echo $value
+done
+# =================================
+for value in {10..0..3}
+do
+echo $value
+done
+# =================================
+val=
+for value in $val/*
+do
+if [ ! -r $value ]
+then
+echo $value IS NOT readable 1>&2
+continue
+fi
+echo $value IS readable 1>&2
+done
+# =================================
+names='Kyle Cartman Stan Quit'
+PS3='Select character: '
+select name in $names
+do
+if [ $name == 'Quit' ]
+then
+break
+fi
+echo Hello $name
+done
