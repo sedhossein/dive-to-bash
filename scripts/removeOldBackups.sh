3a33f7c6d3fc5ece8a795df6196ee60a5a47414e
sedhossein <seyedhosein77@gmail.com>
2020-06-02 01:59:02 +0430

    add remove script

--- scripts/removeOldBackups.sh
+++ scripts/removeOldBackups.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
 isEligibleToRemove() {
+    # backup details
     year=$1
     month=$2
     day=$3
 
-    return 0
+    # today details
+    currentYear=20$(date +"%y")
+    currentMonth=$(date +"%m")
+    currentDay=$(date +"%d")
+
+    # calculate days in both dates
+    backupSeconds=$(($year*364 + $month*30 + $day))
+    todaySeconds=$(($currentYear*364 + $currentMonth*30 + $currentDay))
+
+    diff=$(($todaySeconds-$backupSeconds))
+    if (($diff > $savingDays)) ; then
+        return 0 # 0 = true
+    fi
+
+    return 1 # 1 = false
 }
 
 removeFile() {
@@ -17,10 +32,12 @@ removeFile() {
     read -ra tmp <<< "$filename" # str is read into an array as tokens separated by IFS
 
     if [ "${#tmp[@]}" -eq 3 ]; then
+        
         if isEligibleToRemove "${tmp[0]}" "${tmp[1]}" "${tmp[2]}"; then
-            echo "$filepath removed"
+            echo "$filepath removed!"
+            nice -n 15 rm -rf "$filepath"
         else
-            echo "file $filename skiped!"
+            echo "file $filename skiped."
         fi
     else
         echo "filename has invalid naming: $filepath"
@@ -52,12 +69,7 @@ cleanDirectory() {
 checkList=(/home/sedhossein/tmp/tests1 /home/sedhossein/tmp/tests2 /home/sedhossein/tmp/tests3)
 
 # backup TTLs
-savingDays=3
-
-# current date
-currentYear=20$(date +"%y")
-currentMonth=$(date +"%m")
-currentDay=$(date +"%d")
+savingDays=2
 
 # let's go...
 for dir in ${checkList[@]}; do
